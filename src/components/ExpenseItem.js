f2d83491cd3fb910d99a10461eb4dd866c364502
Felix Chen <fechen1993@gmail.com>
2022-11-22 20:14:48 -0500

    work on ExpenseItem component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import "./ExpenseItem.css";
+
+const ExpenseItem = ({ date, title, amount, id }) => {
+  return (
+    <div className="expense-item">
+      <div>{date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">{amount}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
